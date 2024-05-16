c52cb9094e9196b41d240947ef7dd3f2bd789d99
FarazMalik786 <rashumalik786@gmail.com>
2024-05-16 22:55:06 +0530

    changes

--- src/components/connect-button/ConnectButton.tsx
+++ src/components/connect-button/ConnectButton.tsx
@@ -6,22 +6,24 @@ import { connectWallet, disconnectWallet } from "@/redux/services/walletSlice"
 // redux hooks
 import { useSelector, useDispatch } from 'react-redux'
 import useNetwork from "@/custom-hooks/useNetwork"
-import { useWeb3Modal } from '@web3modal/wagmi/react'
+import { useWalletInfo, useWeb3Modal } from '@web3modal/wagmi/react'
+import { useDisconnect } from 'wagmi'
 
 const ConnectButton = ({ styles }: ConnectProps) => {
   const isConnected = useNetwork()
   const walletState = useSelector((state: RootState) => state.wallet)
+  const { walletInfo } = useWalletInfo()
+  const { disconnect } = useDisconnect()
   const [connectionState, setConnectionState] = useState("not-connected");
   const dispatch = useDispatch<AppDispatch>();
   const { open } = useWeb3Modal()
 
-  const connectingWallet = async () => {
+  const connectingWallet = async () => {    
     try {
-      if (!isConnected) {
+      if (!Boolean(walletInfo)) {
         open()
-
-        //  const userWallet = await dispatch(connectWallet("wallet_switchEthereumChain")) 
-        //  localStorage.setItem("user-wallet", JSON.stringify({ connected: true, wallet: userWallet.payload as string }) );  
+         const userWallet = await dispatch(connectWallet("wallet_switchEthereumChain")) 
+         localStorage.setItem("user-wallet", JSON.stringify({ connected: true, wallet: userWallet.payload as string }) );  
         //  console.log(walletState.user)
         //  window.location.reload();
          setConnectionState("connected")
@@ -32,10 +34,11 @@ const ConnectButton = ({ styles }: ConnectProps) => {
   }
 
   const disconnectingWallet = async () => {
-    if (isConnected) {
+    if (Boolean(walletInfo)) {      
+      disconnect()
       await dispatch(disconnectWallet("wallet_revokePermissions"))
       localStorage.removeItem("user-wallet")
-      window.location.reload();
+      // window.location.reload();
     }
   }
 
@@ -43,9 +46,9 @@ const ConnectButton = ({ styles }: ConnectProps) => {
 
     // ${!isConnected ? "opacity-[1] visible translate-y-[0px]" : "opacity-[0] invisible translate-y-[-100px]"}
     <button
-      onClick={!isConnected ? connectingWallet : disconnectingWallet}
+      onClick={!Boolean(walletInfo) ? connectingWallet : disconnectingWallet}
       className={` ${styles} border-[1px] border-[#D2ECFF] transition-all duration-[0.7s] delay-200 rounded-br-[20px] rounded-tl-[20px] whitespace-nowrap bg-no-repeat bg-contain font-medium outline-none py-[4px] px-[12px]`}>
-      {isConnected ? "Disconnect" : "Connect Wallet"}
+      {Boolean(walletInfo) ? "Disconnect" : "Connect Wallet"}
     </button>
   )
 }
